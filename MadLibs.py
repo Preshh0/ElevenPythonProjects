40612892128e62e761ebe517539f541990f5acef
Preshh0 <uniquep201@gmail.com>
2021-06-19 22:50:18 +0100

    I think this is how th game is played, I'd check. My fav part of this code though, is how i added the string formatting from memory. Yay Me!

---
+++ MadLibs.py
@@ -0,0 +1,6 @@
+Name = input("What's your name? ")
+Age = input("How old are you? ")
+School = input("School name: ")
+Address = input("What's your home address?")
+
+print("Hi, $Name You are $Age and you school at $School and you live at $Address.")
