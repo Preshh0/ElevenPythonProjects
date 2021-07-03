68339c902a2c86a42a9693b7a9a6dda42046886a
Preshh0 <uniquep201@gmail.com>
2021-07-03 11:43:06 +0100

    Finally found a solution! I left the wrong code in still although it is commented out.

--- IsAPalindrome.py
+++ IsAPalindrome.py
@@ -1,8 +1,29 @@
-Sentence = [input("Enter a word: ")]
-res = [i[:: -1] for i in Sentence]
+Sentence = input("Enter a word: ")
 
-if Sentence == res:
+if Sentence == Sentence[::-1]: #::-1 basically just means the inverse whatever variable it is in.
     print(f"{Sentence} is a palindrome.")    
 
 else:
-    print(f"{Sentence} is a palindrome.")
+    print(f"{Sentence} is not palindrome.")
+'''
+        WRONG SOLUTION
+
+# def palindrome():
+#     Sentence = [input("Enter a word: ")]
+#     if len(Sentence) <= 1:
+#         return "This is a plalindrome"
+#     if Sentence[-1] != Sentence[0]:
+#         return "This is not a palindrome"
+
+# palindrome()
+'''
+# def ispal(Sentence):
+#     return Sentence == Sentence[::-1] 
+
+# Sentence = input("input word: ")
+# ans = ispal(Sentence)
+
+# if ans:
+#     print("Yes")
+# else:
+#     print("no")
--- WhatsMyAcronym.py
+++ WhatsMyAcronym.py
@@ -4,12 +4,32 @@
 # Input -> World Health Organization. Output -> WHO.
 # Input -> Absent Without Leave. Output -> AWOL.
 
-string = " Hello, World! "
-print(string.replace("Hello", "Hi"))
-print(string.strip(" ")) #removes the stuff in the quote
-# if "n" != string:
-#     print("Not available")
-# elif "n" in string:
-#     print("AVailable!")
-print(string.split("l")) #the stuff in the quotes indicates where it should cut from. NB: The whitespace is important!
-# print(string1)
+# string = " Hello, World! "
+# print(string.replace("Hello", "Hi"))
+# print(string.strip(" ")) #removes the stuff in the quote
+# # if "n" != string:
+# #     print("Not available")
+# # elif "n" in string:
+# #     print("AVailable!")
+# print(string.split("l")) #the stuff in the quotes indicates where it should cut from. NB: The whitespace is important!
+# # print(string1)\
+
+# name = "Hello World"
+# name.split(" ")
+# for i in name:
+#     for j in name:
+#         print(name[1])
+#     print(name[0])
+
+value = input("Input a value: ")
+ 
+
+if type(value) == str:
+    print("It is a string.")
+if type(value) == int:
+    if value is range (0,99):
+        print("It is an integer.")
+if type(value) == bool:
+    print("It is a boolean.")
+if type(value) == list:
+    print("It is a list.")
